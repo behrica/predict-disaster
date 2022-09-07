15eebff7de37e5296ffa276715ab8b1d7462cc31
Carsten Behring <carsten.behring@gmail.com>
2022-09-07 23:46:33 +0200

    use real data and write result to file

--- train.clj
+++ train.clj
@@ -2,7 +2,8 @@
 
 (ns train
    (:require [libpython-clj2.python :refer [py.- py.] :as py]
-             [libpython-clj2.require :refer [require-python]]))
+             [libpython-clj2.require :refer [require-python]]
+             [scicloj.ml.dataset :as ds]))
 
 
 
@@ -12,38 +13,52 @@
 
 
 
-(def  train-data  [
-                   ["Example sentence belonging to class 1" 1]
-                   ["Example sentence belonging to class 0" 0]])
 
 
 
-(def eval-data  [
-                 ["Example eval sentence belonging to class 1" 1]
-                 ["Example eval sentence belonging to class 0" 0]])
+(def  data
+  (->
+   (ds/dataset "train.csv" {:key-fn keyword})
+   (ds/select-columns [:text :target])
+   (ds/rename-columns {:target :labels})
+   (ds/random 100)))
 
-(py/with-gil-stack-rc-context
-  (let [
-        train-df (pd/DataFrame train-data)
-        eval-df (pd/DataFrame eval-data)
-        model (classification/ClassificationModel
-
-               :use_cuda false
-               :model_type "bert"
-               :model_name "prajjwal1/bert-tiny"
-               :args
-               (py/->py-dict
-
-                {;; :silent true
-                 :dataloader_num_workers 1
-                 :process_count 2
-                 :num_train_epochs 10
-                 :use_multiprocessing false
-                 :overwrite_output_dir true}))
-
-        x (py. model train_model train-df)]
+(def data-split
+  (ds/train-test-split data))
 
 
 
-    (println :x x)
-    (flush)))
+
+
+(def train-result
+  (py/with-gil-stack-rc-context
+    (let [
+          train-df (pd/DataFrame (:train-ds data-split))
+          eval-df  (pd/DataFrame (:test-ds data-split))
+          model (classification/ClassificationModel
+
+                 :use_cuda false
+                 :model_type "bert"
+                 :model_name "prajjwal1/bert-tiny"
+                 :args
+                 { ;; :silent true
+                  :dataloader_num_workers 1
+                  :process_count 2
+                  :num_train_epochs 1
+                  :evaluate_during_training true
+                  :evaluate_during_training_verbose true
+                  :use_multiprocessing false
+                  :overwrite_output_dir true})
+
+          train-result (py. model train_model
+                            train-df
+                            :eval_df eval-df)]
+
+
+
+      (py/->jvm train-result))))
+
+
+
+(spit "train-result.edn"
+      (with-out-str (clojure.data.json/pprint (second  train-result))))
