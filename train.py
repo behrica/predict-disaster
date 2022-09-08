4846b9e7c90433c3213cb4e47281a5b9ad23eb5c
Carsten Behring <carsten.behring@gmail.com>
2022-09-08 17:05:57 +0200

    use params

--- train.py
+++ train.py
@@ -4,8 +4,14 @@ from simpletransformers.classification import ClassificationModel
 import pandas as pd
 import pyarrow as pa
 import pyarrow.feather as feather
+import yaml
+import json
 
-model_args = dict( num_train_epochs=5 )
+with open("params.yaml", 'r') as f:
+    params = yaml.load(f, Loader=yaml.FullLoader)
+
+
+model_args = {"use_cuda": True} | params
 
 model = ClassificationModel("bert",
                             "prajjwal1/bert-tiny",
@@ -14,3 +20,12 @@ model = ClassificationModel("bert",
 train_df = feather.read_feather("train.arrow")
 eval_df =  feather.read_feather("test.arrow")
 model.train_model(train_df,eval_df=eval_df)
+
+eval_result =  model.eval_model(eval_df)
+
+
+with open('eval.json', 'w') as outfile:
+    json.dump(
+        {"train":
+         {"mcc": eval_result[0]['mcc']}},
+        outfile)
