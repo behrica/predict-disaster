114973879e9ebbffe6f0517882de7822b998aa89
Carsten Behring <carsten.behring@gmail.com>
2022-09-08 18:42:33 +0200

    fixed kaggle

--- predict_kaggle.clj
+++ predict_kaggle.clj
@@ -1,16 +1,26 @@
 (ns predict_kaggle
 
   (:require [libpython-clj2.python :refer [py.- py.] :as py]
-     [libpython-clj2.require :refer [require-python]]
-     [scicloj.ml.dataset :as ds]
-     [tech.v3.libs.arrow :as arrow]))
+            [libpython-clj2.require :refer [require-python]]
+            [scicloj.ml.dataset :as ds]
+            [clojure.data.json :as json]
+            [clj-yaml.core :as yaml]
+            [tech.v3.libs.arrow :as arrow]))
 
 (require-python '[simpletransformers.classification
                   :as classification :reload])
 (require-python '[pandas :as pd :reload])
 
+(def params
+  (->
+   (slurp "params.yaml")
+   (yaml/parse-string)
+   :train))
+
 (def model
-  (classification/ClassificationModel "distilbert" "./outputs"))
+  (classification/ClassificationModel
+   (:model_type params)
+   "./outputs"))
 
 
 (def  test
@@ -31,4 +41,4 @@
 (-> test
     (ds/add-column :target prediction)
     (ds/select-columns [:id :target])
-    (ds/write-csv! "submission.csv"))
+    (ds/write-csv! "kaggle_submission.csv"))
