1449b1daf6fc6cac3277ed252b223242935465ec
Carsten Behring <carsten.behring@gmail.com>
2022-09-25 16:21:31 +0200

    working train.clj

--- src/train.clj
+++ src/train.clj
@@ -1,16 +1,13 @@
 (ns train
-  (:require [libpython-clj2.python :refer [py.- py.] :as py]
-            [libpython-clj2.require :refer [require-python]]
-            [scicloj.ml.dataset :as ds]
-            [clojure.data.json :as json]
-            [clj-yaml.core :as yaml]
-            [tablecloth.api :as tc]
-            [preprocess :refer [preprocess]]
-            [tech.v3.libs.arrow :as arrow]))
+  (:require
+   [scicloj.ml.dataset :as ds]
+   [clojure.data.json :as json]
+   [clj-yaml.core :as yaml]
+   [tablecloth.api :as tc]
+   [preprocess :refer [preprocess]]
+   [tech.v3.libs.arrow :as arrow]))
+   
 
-(require-python '[simpletransformers.classification
-                  :as classification])
-(require-python '[pandas :as pd])
 
 (def params
   (->
@@ -20,49 +17,65 @@
 
 (def model-args
   (merge
-   {"use_cuda" false
+
+   {;; "use_multiprocessing" false
+    ;; "process_count" 1
+    "use_cuda" true
     "use_early_stopping" true
     "save_eval_checkpoints" true
-    "evaluate_during_training_steps" 100
+    ;; "evaluate_during_training_steps" 50
     "evaluate_during_training" true
-    "evaluate_during_training_silent" true
+    "evaluate_during_training_silent" false
     "evaluate_during_training_verbose" true}
    params))
 
 
+(require
+         '[libpython-clj2.python :refer [py.- py.] :as py])
+
+(py/initialize! :no-io-redirect? true)
+
+(def pd (py/import-module "pandas"))
+(def st (py/import-module "simpletransformers.classification"))
+
 (def pd-train
-  (->
-   (arrow/stream->dataset "train.arrow" {:key-fn keyword})
-   (tc/select-columns [:text :labels])
-   (tc/head 100)
-   (tc/rows :as-seqs)
-   (pd/DataFrame)))
+  ((py/py.- pd DataFrame)
+   (->
+    (arrow/stream->dataset "train.arrow" {:key-fn keyword})
+    (tc/select-columns [:text :labels])
+    ;; (tc/head 102)
+    (tc/rows :as-seqs))))
 
 (def pd-eval
+  ((py/py.- pd DataFrame)
+   (->
+    (arrow/stream->dataset "test.arrow" {:key-fn keyword})
+    (tc/select-columns [:text :labels])
+    ;; (tc/head 157)
+    (tc/rows :as-seqs))))
+
+(def model ((py.- st ClassificationModel)
+            "bert" "prajjwal1/bert-tiny"
+
+             ;; (:model_type model-args)
+             ;; (:model_name model-args)
+            :use_cuda true
+            :args model-args))
+
+(def train-result  (py. model train_model pd-train :eval_df pd-eval))
+
+(def eval-result
   (->
-   (arrow/stream->dataset "test.arrow" {:key-fn keyword})
-   (tc/select-columns [:text :labels])
-   (tc/head)
-   (tc/rows :as-seqs)
-   (pd/DataFrame)))
+   (py. model eval_model pd-eval)
+   (py/->jvm)))
 
-(def model
-  (classification/ClassificationModel
-   (:model_type model-args)
-   (:model_name model-args)
-   :args model-args))
+(spit "eval.json"
+ (json/write-str
+  {:train
 
-
-
-(def _  (py. model train_model pd-train :eval_df pd-eval))
+   (-> eval-result first (select-keys ["mcc"]))}))
 
 (println "training finished")
 (shutdown-agents)
 
 
-(comment
-
-  (System/gc)
- (require '[libpython-clj2.python.gc])
- (libpython-clj2.python.gc/clear-reference-queue)
- (System/gc))
