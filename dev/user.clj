0d321152da44100113f8a8a914e54e1e70461360
Carsten Behring <carsten.behring@gmail.com>
2022-10-05 21:20:34 +0200

    started analysis

---
+++ dev/user.clj
@@ -0,0 +1,12 @@
+(ns user)
+  ;; (:require [scicloj.clay.v1.api :as clay]
+  ;;           [scicloj.clay.v1.tools :as tools]
+  ;;           [scicloj.clay.v1.extensions :as extensions]
+  ;;           [scicloj.clay.v1.tool.scittle :as scittle]
+  ;;           [scicloj.kindly.v2.api :as kindly]
+  ;;           [scicloj.kindly.v2.kind :as kind]
+  ;;           [scicloj.kindly.v2.kindness :as kindness]
+  ;;           [nextjournal.clerk :as clerk])
+
+
+;; (clay/start! {:tools [tools/clerk] :extensions [extensions/dataset]})
