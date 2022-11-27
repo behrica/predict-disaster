48e62d278e041f5649c040312f451b88d85e1170
Carsten Behring <carsten.behring@gmail.com>
2022-11-27 15:38:45 +0100

    added launchpad
    added LIME test

--- dev/user.clj
+++ dev/user.clj
@@ -1,12 +1,16 @@
-(ns user)
-  ;; (:require [scicloj.clay.v1.api :as clay]
-  ;;           [scicloj.clay.v1.tools :as tools]
-  ;;           [scicloj.clay.v1.extensions :as extensions]
-  ;;           [scicloj.clay.v1.tool.scittle :as scittle]
-  ;;           [scicloj.kindly.v2.api :as kindly]
-  ;;           [scicloj.kindly.v2.kind :as kind]
-  ;;           [scicloj.kindly.v2.kindness :as kindness]
-  ;;           [nextjournal.clerk :as clerk])
+(ns user
+  (:require [org.scicloj.casagemas.setup]))
+
+(org.scicloj.casagemas.setup/start-clerk!)
+
+;; (:require [scicloj.clay.v1.api :as clay]
+;;           [scicloj.clay.v1.tools :as tools]
+;;           [scicloj.clay.v1.extensions :as extensions]
+;;           [scicloj.clay.v1.tool.scittle :as scittle]
+;;           [scicloj.kindly.v2.api :as kindly]
+;;           [scicloj.kindly.v2.kind :as kind]
+;;           [scicloj.kindly.v2.kindness :as kindness]
+;;           [nextjournal.clerk :as clerk])
 
 
 ;; (clay/start! {:tools [tools/clerk] :extensions [extensions/dataset]})
--- src/predict_kaggle.clj
+++ src/predict_kaggle.clj
@@ -11,7 +11,10 @@
 
 (require-python '[simpletransformers.classification
                   :as classification])
-(require-python '[pandas :as pd])
+;; (require-python '[pandas :as pd])
+;;
+;;
+
 
 (defn predict-kaggle [_]
   (let [params
@@ -43,4 +46,96 @@
           (ds/write-csv! "kaggle_submission.csv"))))
 
 
-(comment (predict-kaggle nil))
+(comment
+
+
+  (def params
+    (merge
+     {:use_multiprocessing false
+      :process_count 1
+      :use_cuda true}
+
+     (->
+      (slurp "params.yaml")
+      (yaml/parse-string)
+      :train)))
+
+
+
+
+  (def model
+    (classification/ClassificationModel
+     (:model_type params)
+     "./outputs/best_model/"))
+
+  (py/from-import lime.lime_text LimeTextExplainer)
+
+
+  (def explainer (LimeTextExplainer :class_names ["fake" "real"]))
+  (def numpy (py/import-module "numpy"))
+
+
+  (py/from-import scipy.special softmax)
+  (def predict-proba-fn
+
+    (py/make-callable (fn [texts]
+                        (let [soft-max
+                              (->
+                               (py. model predict (py/->py-list texts))
+                               second
+                               (softmax :axis -1))]
+
+                          (py. numpy array (mapv first soft-max))))))
+
+
+  (def test-ds (->
+                (ds/dataset "test.csv" {:key-fn keyword})
+                preprocess))
+
+  (def train-ds (->
+                 (ds/dataset "train.csv" {:key-fn keyword})
+                 preprocess))
+
+  (def real
+    (-> train-ds
+        (tc/select-rows   (fn [row] (= 1 (:labels row))))
+        :text
+        second))
+
+  (def fake
+    (-> train-ds
+        (tc/select-rows   (fn [row] (= 0 (:labels row))))
+        :text
+        (nth 33)))
+
+
+  (->
+   (py/py. explainer explain_instance fake predict-proba-fn)
+   (py/py. save_to_file "/tmp/fake.html"))
+
+  (->
+   (py/py. explainer explain_instance real predict-proba-fn)
+   (py/py. save_to_file "/tmp/real.html"))
+
+
+
+
+
+
+
+  (py/->jvm
+   (py/cfn predict-proba-fn ["hello" "world"]))
+                     
+  (def builtins (py/import-module "builtins"))
+
+  (py. builtins type xxx)
+
+  (def main-globals (-> (py/add-module "__main__")
+                        (py/module-dict)))
+
+  (.put main-globals "xxx" xxx)
+
+  (py/run-simple-string "print(xxx('hello'))")
+  (py/run-simple-string "print(type(xxx))")
+
+  :ok)
